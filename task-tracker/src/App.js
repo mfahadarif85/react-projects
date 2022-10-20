e2cd090c096f5c109a6ad092fac9d0d08e9d6467
mfahadarif85 <mfahadarif@gmail.com>
2022-10-20 10:49:34 +0500

    task-tracker: showAddTask state

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -39,9 +41,17 @@ function App() {
     );
   };
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div className='container'>
       <Header />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask} />
       ) : (
