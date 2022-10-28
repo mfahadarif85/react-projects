7eda47ea67e0c418d6e2a3636071df666b8d2978
mfahadarif85 <mfahadarif@gmail.com>
2022-10-28 20:18:53 +0500

    brian: intro-to-react-Day1part3

---
+++ brianholt-react/adopt-me/src/SearchParams.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["poodle"];
+  return (
+    <div className='search-params'>
+      <form>
+        <label htmlFor='location'>
+          Location
+          <input
+            type='text'
+            id='location'
+            value={location}
+            placeholder='Location'
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor='animal'>
+          Animal
+          <select
+            name='animal'
+            id='animal'
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor='breed'>
+          Breed
+          <select
+            name='breed'
+            id='breed'
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
