c519be1f4ccacbf6ea147a5eb6f4a5b22f254099
mfahadarif85 <mfahadarif@gmail.com>
2022-11-02 10:21:46 +0500

    brianholt: intro-to-react: error boundaries

--- brianholt-react/adopt-me/src/Details.js
+++ brianholt-react/adopt-me/src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -16,6 +17,9 @@ class Details extends Component {
     if (this.state.loading) {
       return <h2>Loading...</h2>;
     }
+
+    // throw new Error("lmao you crashed");
+
     const { animal, breed, city, state, description, name, images } =
       this.state;
     return (
@@ -36,7 +40,11 @@ class Details extends Component {
 
 const WrappedDetails = () => {
   const params = useParams();
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />
+    </ErrorBoundary>
+  );
 };
 
 export default WrappedDetails;
