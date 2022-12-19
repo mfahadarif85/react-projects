ba36a5d6645ab38794590342471beb74e0c85229
mfahadarif85 <mfahadarif@gmail.com>
2022-12-19 18:57:54 +0500

    stephen: sec13

--- stephen-react/comps/src/index.js
+++ stephen-react/comps/src/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import { NavigationProvider } from "./context/navigation";
+import App from "./App";
+
 const rootNode = document.getElementById("root");
 const root = ReactDOM.createRoot(rootNode);
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
