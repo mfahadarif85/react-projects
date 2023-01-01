1877d9a90141fbcb7a0f139186bf530295f7f396
mfahadarif85 <mfahadarif@gmail.com>
2023-01-01 14:04:29 +0500

    steph-react-sec20-327

---
+++ stephen-react/cars/src/index.js
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./App";
+
+const rootNode = document.getElementById("root");
+const root = createRoot(rootNode);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
