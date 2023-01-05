b1b1204a5f5a5567069a1c0d154537d079b13fa3
mfahadarif85 <mfahadarif@gmail.com>
2023-01-05 20:23:51 +0500

    brian-react-lesson2

---
+++ brianholt-react/adopt-me/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Bird",
+      animal: "Pepper",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Cat",
+      animal: "Doink",
+      breed: "Mixed",
+    }),
+  ]);
+};
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
