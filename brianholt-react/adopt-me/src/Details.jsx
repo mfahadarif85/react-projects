7dfc7c3d3f395b26c1cb00ba6d89d18408387f69
mfahadarif85 <mfahadarif@gmail.com>
2023-01-07 13:21:19 +0500

    brian-react-lesson6

--- brianholt-react/adopt-me/src/Details.jsx
+++ brianholt-react/adopt-me/src/Details.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
 
 function Details() {
@@ -15,6 +16,7 @@ function Details() {
   const pet = results.data.pets[0];
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
